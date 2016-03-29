11204e5a63821ffbd85bd6484c9b1591753c42b4
Elise <scolaro.elise@gmail.com>
2016-03-29 19:06:27 +0200

    -Facebook login implementation

--- dist/components/headApp.component.js
+++ dist/components/headApp.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2-jwt'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,17 +10,39 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, angular2_jwt_1;
     var headAppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (angular2_jwt_1_1) {
+                angular2_jwt_1 = angular2_jwt_1_1;
             }],
         execute: function() {
             headAppComponent = (function () {
                 function headAppComponent() {
+                    this.lock = new Auth0Lock('1kkbLOIrJR8dZw22yP96MSqWrRXQmZl7', 'elisescolaro.eu.auth0.com');
+                    console.log(this.lock);
                 }
+                headAppComponent.prototype.login = function () {
+                    console.log("login");
+                    this.lock.show(function (err, profile, id_token) {
+                        if (err) {
+                            throw new Error(err);
+                        }
+                        localStorage.setItem('profile', JSON.stringify(profile));
+                        localStorage.setItem('id_token', id_token);
+                    });
+                };
+                headAppComponent.prototype.logout = function () {
+                    localStorage.removeItem('profile');
+                    localStorage.removeItem('id_token');
+                };
+                headAppComponent.prototype.loggedIn = function () {
+                    return angular2_jwt_1.tokenNotExpired();
+                };
                 headAppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-head',
