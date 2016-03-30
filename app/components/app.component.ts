25ee52733535c4b139bf240c0a902d32e48e3fc9
jessy-dupin <jessy.dupin@isobar.com>
2016-03-30 11:52:08 +0200

    mise en place btn login

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -5,12 +5,11 @@ import {headAppComponent} from './headApp.component';
 import {subHeadAppComponent} from './subHeadApp.component';
 import {firstPageComponent} from './firstPage/firstPage.component';
 import {secondPageComponent} from './secondPage/secondPage.component';
-import {loginAppComponent} from './loginApp.component';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/templates/app.html',
-    directives: [headAppComponent, subHeadAppComponent, firstPageComponent, secondPageComponent, loginAppComponent]
+    directives: [headAppComponent, subHeadAppComponent, firstPageComponent, secondPageComponent]
     //directives: [headAppComponent, subHeadAppComponent, firstPageComponent, loginAppComponent, [ROUTER_PROVIDERS]]
 })
 
--- app/components/loginApp.component.ts
+++ app/components/loginApp.component.ts
@@ -1,8 +1,41 @@
 import {Component} from 'angular2/core';
 
+import {RouteConfig, Router, APP_BASE_HREF, ROUTER_PROVIDERS, ROUTER_DIRECTIVES, CanActivate} from 'angular2/router';
+import {HTTP_PROVIDERS, Http} from 'angular2/http';
+import {AuthHttp, tokenNotExpired, JwtHelper} from 'angular2-jwt';
+
 @Component({
     selector: 'my-login',
     templateUrl: 'app/templates/login.html'
 })
 
-export class loginAppComponent {}
+export class loginAppComponent {
+	lock = new Auth0Lock('1kkbLOIrJR8dZw22yP96MSqWrRXQmZl7', 'elisescolaro.eu.auth0.com');
+
+	constructor() {
+		console.log(this.lock);
+	}
+
+	login() {
+		console.log("login");
+		this.lock.show(function(err:string, profile:string, id_token:string) {
+
+		if(err) {
+			throw new Error(err);
+		}
+
+		localStorage.setItem('profile', JSON.stringify(profile));
+		localStorage.setItem('id_token', id_token);
+
+		});
+	}
+
+	logout() {
+		localStorage.removeItem('profile');
+		localStorage.removeItem('id_token');
+	}
+
+	loggedIn() {
+		return tokenNotExpired();
+	}
+}
